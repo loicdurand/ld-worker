b29c6971adaa7e3432bfbbfed00a8fc7e9d7d369
Loic DURAND <loicdurand.ld@gmail.com>
2022-07-26 14:52:11 +0200

    Lib is OK

--- index.js
+++ index.js
@@ -1,38 +1,27 @@
 
 export default (fn) => {
 
-    if (window.Worker) {
-
-        const //
-            code = (worker, fn) => {
-                onmessage = function (e) {
-                    const // 
-                        { data } = e,
-                        args = data,
-                        result = fn(...args);
-                    worker.postMessage(result);
-                }
-            },
-            str = ` (${code.toString()})(self, ${fn.toString()})`,
-            blob = new Blob([str], { type: 'application/javascript' }),
-            worker = new Worker(URL.createObjectURL(blob));
-
-        return (...args) => new Promise((resolve, reject) => {
-            worker.postMessage(args);
-            worker.onmessage = ({ data: result }) => resolve(result);
-            worker.onerror = e => reject(e);
-        });
-
-    } else {
-        return (...args) => new Promise((resolve, reject) => {
+    if (!window.Worker)
+        return (...args) => new Promise((res, rej) => {
             try {
-                const result = fn(...args);
-                resolve(result);
-            } catch (error) {
-                reject(error);
+                res(fn(...args));
+            } catch (err) {
+                rej(err);
             }
         });
-    }
+
+    const //
+        code = (w, fn) => onmessage = ({ data: args }) => w.postMessage(fn(...args)),
+        arr = ['(', code.toString(), ')(self,', fn.toString(), ')'],
+        blob = new Blob(arr, { type: 'application/javascript' }),
+        worker = new Worker(URL.createObjectURL(blob));
+
+    return (...args) => new Promise((resolve, reject) => {
+        worker.postMessage(args);
+        worker.onmessage = ({ data: result }) => resolve(result);
+        worker.onerror = e => reject(e);
+    });
+
 
 }
 
