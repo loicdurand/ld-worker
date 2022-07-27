e50fb6394a8d1f8e26822891e5d0157807086ac0
Loic DURAND <loicdurand.ld@gmail.com>
2022-07-27 10:42:11 +0200

    légère correction

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
-
 export default (fn) => {
 
-    if (!window.Worker)
-        return (...args) => new Promise((res, rej) => {
-            try {
-                res(fn(...args));
-            } catch (err) {
-                rej(err);
-            }
-        });
+    return (...args) => {
+        if (!window.Worker)
+            return (...args) => new Promise((res, rej) => {
+                requestAnimationFrame(() => {
+                    try {
+                        res(fn(...args));
+                    } catch (err) {
+                        rej(err);
+                    }
+                });
+            });
 
-    const //
-        code = (w, fn) => onmessage = ({ data: args }) => w.postMessage(fn(...args)),
-        arr = ['(', code.toString(), ')(self,', fn.toString(), ')'],
-        blob = new Blob(arr, { type: 'application/javascript' }),
-        worker = new Worker(URL.createObjectURL(blob));
+        const //
+            listener = (resolve) =>({ data: result }) =>  resolve(result),
+            code = (w, fn) => onmessage = ({ data: args }) => w.postMessage(fn(...args)),
+            arr = ['(', code.toString(), ')(self,', fn.toString(), ')'],
+            blob = new Blob(arr, { type: 'application/javascript' }),
+            worker = new Worker(URL.createObjectURL(blob));
 
-    return (...args) => new Promise((resolve, reject) => {
         worker.postMessage(args);
-        worker.onmessage = ({ data: result }) => resolve(result);
-        worker.onerror = e => reject(e);
-    });
-
-
+        return new Promise((resolve, reject) => {
+            worker.addEventListener('message', listener(resolve))
+            worker.onerror = e => reject(e);
+        });
+    }
 }
 
